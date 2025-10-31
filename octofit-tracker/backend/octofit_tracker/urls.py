6666fa936300cdea48fb9da5c257d445414c3d72
ZouhairElkili <137197509+zelkili@users.noreply.github.com>
2025-10-31 13:58:28 +0000

    Ajout de la structure de base de l'application Django avec les fichiers de configuration, les modèles, les vues et les commandes de gestion pour la connexion à MongoDB et le peuplement de la base de données.

---
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -0,0 +1,37 @@
+"""octofit_tracker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        "message": "Bienvenue sur l'API Octofit Tracker!",
+        "endpoints": [
+            "/users/",
+            "/teams/",
+            "/activities/",
+            "/leaderboard/",
+            "/workouts/"
+        ]
+    })
+
+urlpatterns = [
+    path('', api_root, name='api_root'),
+    path('admin/', admin.site.urls),
+]
